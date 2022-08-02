1f7af4bfe7c23a2dee6cffa3004fb6cc266d1e45
Ghayath <ghayath94sh@gmail.com>
2022-08-02 18:17:44 +0200

    conditons

---
+++ conditions/task2.py
@@ -0,0 +1,34 @@
+x=10
+
+if (x>15):print('Not')
+else:print('x is smaller than 15')
+'''
+
+(3)->Use all() when you need to check a long series of and conditions.
+(4)->The same is the shortening and beautifying of the code.
+(5)->The same is the shortening and beautifying of the code.
+(6)->all
+(7)->The main part of if is only executed when evaluated to True
+The elif is the abbreviation for otherwise if.
+It allows us to search for multiple expressions.
+(8)->The elif statement allows you to check multiple
+expressions for TRUE and execute a block of code
+as soon as one of the conditions evaluates to TRUE
+An else statement can be combined with an if statement.
+An else statement contains the block of code that executes
+if the conditional expression in the if statement resolves to 0 or a FALSE value.
+
+
+(9)->yes
+(10)->No
+(11)->
+(12)->No
+(13)->multiple expressions.
+
+'''
+l=[2,4,6,88,10]
+print('yes')if 4 in l else print('No')
+print('yes')if all ([4,6 in l])else print('NO')
+print('Yes')if any([3, 6 in l])else print('NO')
+Print('Yes')if all([2 , 4 , 5 in l])else print('No')
+
