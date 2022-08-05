e805621f604b6c8f32d03d9cc8fb93f47e55486b
Ghayath <ghayath94sh@gmail.com>
2022-08-06 01:03:31 +0200

    Python3

---
+++ task3/Loops.py
@@ -0,0 +1,85 @@
+'''
+#(1)
+x=0
+while x<11:
+    print(x)
+    x+=1
+
+    
+#(2)
+   
+for l in range(0,11):
+    print(l)
+
+#(3)
+
+for p in range(0,11):
+ 
+ if p==5:break
+ print(p)
+
+#(4)
+for k in range(1,11):
+ 
+ if k==5:continue
+ print(k)
+
+
+#(5)
+n=5
+for v in range(1,6):
+    print(n,'X',v,'=',n*v)
+
+
+#(5)
+def Multi():
+    for s in range(1, 6):
+
+        print(" ")
+
+        for a in range(1, 6):
+            print(s*a,end=" ")
+
+Multi()
+
+#(6)
+for i in range(10,21):
+    print(i)
+
+print('----------------------------------------------------')
+#(7)
+for d in range(10,101,3):
+    print(d)
+
+
+
+#(8)
+
+for list in range(1,101):
+    if list %2==0:
+   
+    
+        print(list)
+        
+
+
+
+#(9)
+
+h=2
+while h<=100:
+    print(h)
+    
+    h+=2
+   
+
+ #(10)
+ for m in range(1,101):
+    if m %2==0:
+    
+        print(list)
+'''
+
+
+
+
